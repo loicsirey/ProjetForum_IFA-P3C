8c3cf889fde43eccae2b64acdd65f185b4f45f5e
cedric <cedric.drrnm@eduge.ch>
2017-04-07 10:39:03 +0200

    First Commit

---
+++ Subject_AND_CreateCommentSubject/WPF_SujetForum/DAO.cs
@@ -0,0 +1,159 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_SujetForum
+{
+    class DAO
+    {
+        MySqlConnection conn;
+        public DAO()
+        {
+            string server = "localhost";
+            string database = "forumcs";
+            string uid = "adminForumCS";
+            string password = "Super2012";
+
+            conn = new MySqlConnection("SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";");
+        }
+
+        //Open connection
+        public bool OpenConnection()
+        {
+            try
+            {
+                conn.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Impossible de se connecter à la base de données");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Nom d'utilisateur ou mot de passe invalides !");
+                        break;
+
+                    default:
+                        MessageBox.Show("Erreur lors de la connection");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                conn.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        public void InsertSubject(string nomSubject)
+        {
+            string query = "INSERT INTO `subject`(`nomSubject`) VALUES (@nomSubject);";
+
+            //open connection
+            if (OpenConnection())
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@nomSubject", nomSubject);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                CloseConnection();
+            }
+        }
+
+        //Select statement
+        public Dictionary<string, string> SelectAllSubject()
+        {
+            string query = "SELECT * FROM subject";
+
+            //Create a list to store the result
+            Dictionary<string, string> dictionnaire = new Dictionary<string, string>();
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    dictionnaire.Add(dataReader["idSubject"].ToString(), dataReader["nomSubject"].ToString());
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                this.CloseConnection();
+
+                //return list to be displayed
+                return dictionnaire;
+            }
+            else
+            {
+                return dictionnaire;
+            }
+        }
+
+        public void DeleteById(string idSubject)
+        {
+            string query = "DELETE FROM comment WHERE idSubject = @idSubject; DELETE FROM subject WHERE idSubject = @idSubject;";
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@idSubject", idSubject);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+        public void InsertComment(string comment, string idSubject, string idUser)
+        {
+            string query = "INSERT INTO `comment`(`textComment`, `idSubject`, `idUser`) VALUES (@textComment, @idSubject, @idUser);";
+
+            //open connection
+            if (OpenConnection())
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@textComment", comment);
+                cmd.Parameters.AddWithValue("@idSubject", idSubject);
+                cmd.Parameters.AddWithValue("@idUser", idUser);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                CloseConnection();
+            }
+        }
+    }
+}
---
+++ Subject_AND_CreateCommentSubject/WPF_SujetForum/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_SujetForum
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DAO daoConnection;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            daoConnection = new DAO();
+            RefreshListBox();
+        }
+
+        private void btnCreateSujet_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbxCreateSujet.Text != "")
+            {
+                daoConnection.InsertSubject(tbxCreateSujet.Text);
+                RefreshListBox();
+            }
+        }
+
+        private void btnDeleteSujet_Click(object sender, RoutedEventArgs e)
+        {
+            if (lsbSujets.SelectedItem != null)
+            {
+                daoConnection.DeleteById(lsbSujets.SelectedItem.ToString().Split('.')[0]);
+                RefreshListBox();
+            }
+        }
+
+        private void RefreshListBox()
+        {
+            lsbSujets.Items.Clear();
+            Dictionary<string, string> tempDictionnaire = daoConnection.SelectAllSubject();
+            List<string> listKeys = tempDictionnaire.Keys.ToList();
+            List<string> listValues = tempDictionnaire.Values.ToList();
+            for (int i = 0; i < listKeys.Count; i++)
+            {
+                lsbSujets.Items.Add(listKeys[i] + "." + listValues[i]);
+            }
+        }
+
+        private void btnCreateCommentaire_Click(object sender, RoutedEventArgs e)
+        {
+            if (lsbSujets.SelectedItem != null && tbxComments.Text != "")
+            {
+                daoConnection.InsertComment(tbxComments.Text, lsbSujets.SelectedItem.ToString().Split('.')[0], "1");
+            }
+        }
+    }
+}
